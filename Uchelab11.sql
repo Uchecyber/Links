de9acc0cdeee519197c5e376a896e995cc42f0e9
Uche <Lindaobikwelu91@gmail.com>
2024-11-16 17:53:46 +0100

    Create Uchelab11.sql

---
+++ Uchelab11.sql
@@ -0,0 +1,10 @@
+---Assignments for a Specific Course---
+SELECT title, due_date FROM assignments WHERE course_id = 'COMP1234';
+---Latest Assignment Due Date---
+SELECT * FROM assignments where due_date = '2025-03-20';
+---Assignments Due on a Specific Date---
+SELECT title, course_id FROM assignments WHERE due_date = '2024-10-08';
+---Find All Assignments Due in October---
+SELECT title, due_date FROM assignments WHERE due_date LIKE '2024-10%';
+---Find the Most Recent Completed Assignment---
+SELECT * FROM assignments WHERE due_date = '2025-03-20';
